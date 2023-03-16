d9688a075a16f1ce2ed417e9aa16d2044892ce26
Kobe Alexander <koalexander@my365.bellevue.edu>
2023-03-15 19:47:15 -0500

    Adding Module 1

---
+++ Module_1/HelloWorldApp/AlexanderHelloWorld.java
@@ -0,0 +1,24 @@
+/*
+Alexander, K. (2023). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved. 
+*/
+
+package Module_1.HelloWorldApp;
+
+public class AlexanderHelloWorld {
+
+/**
+ * Method with one argument
+ * @param args
+ */    
+    public static void main(String[] args) {
+
+    
+//Setting the two string variables
+    String firstName = "Kobe";
+    String lastName = "Alexander";
+
+//Using printf to write the two string variables to the System.out window
+    System.out.printf("Hello World from %s %s", firstName, lastName);
+
+    }
+}
